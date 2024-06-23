9139e57a8c957d7720e2741d718a325f509f5ee4
Liam Sheehy <20104118@mail.wit.ie>
2024-06-23 21:40:16 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -3,9 +3,29 @@ import PageTemplate from '../components/templateMovieListPage';
 import { BaseMovieProps } from "../types/interfaces";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import AddToFavouritesIcon from '../components/cardIcons/addToFavourites';
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+import Spinner from "../components/spinner";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const UpcomingMoviesPage: React.FC = () => {
   const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [titleFiltering, genreFiltering]
+  );
 
   useEffect(() => {
     getUpcomingMovies().then(movies => {
@@ -13,12 +33,34 @@ const UpcomingMoviesPage: React.FC = () => {
     });
   }, []);
 
+  const changeFilterValues = (type: string, value: string) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
+  const displayedMovies = filterFunction(movies);
+
+  if (movies.length === 0) {
+    return <Spinner />;
+  }
+
   return (
-    <PageTemplate
-      title='Upcoming Movies'
-      movies={movies}
-      action={(movie: BaseMovieProps) => <AddToFavouritesIcon {...movie} />}
-    />
+    <>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={displayedMovies}
+        action={(movie: BaseMovieProps) => <AddToFavouritesIcon {...movie} />}
+      />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
 
