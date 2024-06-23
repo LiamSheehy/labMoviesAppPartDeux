b00f2fa691bab1931f02eb05a108d28fe2942b72
Liam Sheehy <20104118@mail.wit.ie>
2024-06-23 12:52:03 +0100

    Added Upcoming movies page.

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -6,7 +6,7 @@ export const getMovies = () => {
       .then(json => json.results);
   };
   
-  export const getMovie = ( id : string) => {
+  export const getMovie = (id: string) => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
     ).then(res => res.json());
@@ -14,29 +14,32 @@ export const getMovies = () => {
   
   export const getGenres = () => {
     return fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        import.meta.env.VITE_TMDB_KEY +
-        "&language=en-US"
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US`
     )
       .then(res => res.json())
       .then(json => json.genres);
   };
   
-   export const getMovieImages = ( id : string | number) => {
+  export const getMovieImages = (id: string | number) => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
     )
-      .then((res) => res.json())
-      .then((json) => json.posters);
+      .then(res => res.json())
+      .then(json => json.posters);
   };
-
-  export const getMovieReviews = (id: string | number) => { //movie id can be string or number
+  
+  export const getMovieReviews = (id: string | number) => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
     )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+  
+  export const getUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
   };
